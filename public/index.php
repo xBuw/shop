37fa12c75cd184b17651329108e6b04464f04148
xBuw <kirichenko_s@bk.ru>
2017-03-15 18:13:40 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xbuw
+ * Date: 15.03.17
+ * Time: 17:55
+ */
+use xbuw\framework\Application;
+
+$loader = require '../vendor/autoload.php';
+
+$page = new Application();
+echo "hi from shop index</br>";
