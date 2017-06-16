b6e0e8d5f8784fd95de675ebf91e01f68588ab66
xBuw <kirichenko_s@bk.ru>
2017-06-16 03:52:10 +0300

    Add new ValidationRule

---
+++ src/ValidationRules/MaxValidationRule.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: xbuw
+ * Date: 14.06.17
+ * Time: 15:23
+ */
+namespace Shop\ValidationRules;
+
+use xbuw\framework\Validation\AbstractValidationRule;
+
+class MaxValidationRule extends AbstractValidationRule
+{
+
+    function check(string $field_name, $field_value, array $params): bool
+    {
+        return floatval($field_value) <= floatval($params[0]);
+    }
+
+    public function getError(string $field_name, $field_value, array $params): string
+    {
+        return "$field_name field must be less than ".$params[0];
+    }
+}
